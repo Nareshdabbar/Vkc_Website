21ceb684353fc39106f9fe1b07724272cd1f3359
Dabbar Naresh <nareshdabar@gmail.com>
2022-12-20 16:29:09 +0530

    changing slider

--- vkc_project/src/component/home/slider/Slider.component.js
+++ vkc_project/src/component/home/slider/Slider.component.js
@@ -59,14 +59,14 @@ function HomeFirstSlider() {
               <div key={id} className="bg-light" >
                 <div className="row">
                   <div className="  col-sm-12 col-md-6   col-lg-5">
-                    <div className="col-lg-10">
+                    <div className="col-lg-10 m-4 p-4 text-center">
                       <h1 className="title" >{item.title}</h1>
                       <h3 className="s-title">{item.subTitle}</h3>
                       <p className="s-desc">{item.descrption}</p>
                       <a href={item.link} target="_blank" type="button" className="btn btn-primary a">Go <BsArrowRight /> </a>
                     </div>
                   </div>
-                  <div className=" d-flex justify-content-center bg-dark col-sm-12 col-md-5 col-lg-6 ">
+                  <div className=" d-flex justify-content-center  col-sm-12 col-md-5 col-lg-6 ">
                     <img src={item.img} className="img-fluid img1"   ></img>
                   </div>
                 </div>
