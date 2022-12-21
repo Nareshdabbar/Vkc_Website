a065f83470306406d0f58fe733b657c1e3f10bcf
Revanth <irevanth54@gmail.com>
2022-12-21 21:32:52 +0530

    add some changes

--- vkc_project/src/App.js
+++ vkc_project/src/App.js
@@ -2,8 +2,6 @@
 import HomePage from "./component/home/homepage/homepage.component";
 import Home from "./component/home/Home.component";
 import Student from "./component/studentreview-partners/student.com";
-
-
 function App() {
   return (
     <div >
