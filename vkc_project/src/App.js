30d375b2fd81512bcf7ab09e7979bea75732da99
Dabbar Naresh <nareshdabar@gmail.com>
2022-12-21 16:33:13 +0530

    delete and slider adding

--- vkc_project/src/App.js
+++ vkc_project/src/App.js
@@ -1,11 +1,11 @@
 import Home from "./component/home/Home.component";
-import NavbarHeader from "./component/navbar/Navbar.component";
+
 
 
 function App() {
   return (
     <div className="">
-{/* <NavbarHeader/> */}
+
 <Home/>
     </div>
   );
--- vkc_project/src/component/ravali/newcom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Newcom() {
-  return (
-    <div>haii</div>
-  )
-}
-
-export default Newcom
