1d2da969a5f2dc285bd0c3061b488b822e78386d
Revanth <irevanth54@gmail.com>
2022-12-22 15:54:37 +0530

    added some changes

--- vkc_project/src/App.js
+++ vkc_project/src/App.js
@@ -1,11 +1,12 @@
-
 import HomePage from "./component/home/homepage/homepage.component";
 import Home from "./component/home/Home.component";
 import Student from "./component/studentreview-partners/student.com";
+import LogoComponent from "./component/logoandicons/logo.component";
 function App() {
   return (
     <div >
   <HomePage/>
+  <Student/>
     </div>
   );
 }
--- vkc_project/src/component/logoandicons/logo.component.js
+++ vkc_project/src/component/logoandicons/logo.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import vkclogo from '../effects/vkc-trainings-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faFacebook,faTelegram,faTwitter,faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faFacebook,faInstagram,faTwitter,faYoutube} from '@fortawesome/free-brands-svg-icons';
 import './logo.css';
 function LogoComponent() {
   return (
@@ -9,16 +9,30 @@ function LogoComponent() {
     <div>
         <img className="logo" src={vkclogo}></img>
     </div>
-    <div className='singleCol social-media-icon d-flex justify-content-evenly'>
-        <a href="http://facebook.com">
+    <div>
+        <h5 id="s-font">Follow Us On</h5>
+    </div>
+    <div className='social-media-icon d-flex'>
+        <div className='m-2'>
+        <a href ="http://facebook.com">
             <FontAwesomeIcon icon={faFacebook}/>
         </a>
+        </div>
+        <div className='m-2'>
         <a href="http://youtube.com">
         <FontAwesomeIcon icon={faYoutube} />
         </a>
+        </div>
+        <div className='m-2'>
         <a href="http://twitter.com">
             <FontAwesomeIcon icon={faTwitter}/>
         </a>
+        </div>
+        <div className='m-2'>
+        <a href="http://instagram.com">
+            <FontAwesomeIcon icon={faInstagram}/>
+        </a>
+        </div>
     </div>
 </div>
   )
