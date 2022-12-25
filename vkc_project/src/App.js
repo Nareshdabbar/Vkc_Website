d8bf501b0e87281b9fb02776d3512edb53084c01
Dabbar Naresh <nareshdabar@gmail.com>
2022-12-25 22:13:38 +0530

    modified naresh

--- vkc_project/src/App.js
+++ vkc_project/src/App.js
@@ -1,19 +1,12 @@
-import HomePage from "./component/home/homepage/homepage.component";
+
 import Home from "./component/home/Home.component";
-// import LogoComponent from "./component/logoandicons/logo.component";
-
-// import Logo from "./component/home/footer/logo/Logo";
-import Slides from "./component/slides/slides.component";
-// import Student from "./component/home/studentreview-partners/student.com";
-
-
 
 function App() {
   return (
     <div className="">
 
 <Home/>
-{/* <Slides/> */}
+
     </div>
   )
 }
